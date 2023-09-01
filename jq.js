62836cf5f02bc85938ac0b453defa944e0ff2094
AntikristeenA <69676969+AntikristeenA@users.noreply.github.com>
2023-09-01 08:54:09 +0200

    Add files via upload

---
+++ jq.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+  // $("ul").css("border", "black");
+  $("li").css("border", "black");
+});
+
+
+// $(document).ready(function() {
+  // $(window).scroll(function() {
+		// if ($(document).scrollTop() > 50) {
+		  // $("#bcgr").animate({
+			// height:'100px'
+			
+		  // });
+		  // $("#ttl0").css("opacity","1");
+		// } else {
+		  // $("#bcgr").animate({
+			// height:'300px'
+		  // });
+		  // $("#ttl0").css("opacity","0");
+		// };
+	// });
+// });
+// $(document).ready(function(){
+	
+	
+	// let aud = document.getElementById("myVideo");
+	
+	
+
+
+	
+  // $("#pl1").click(function(){
+	// let aud = document.getElementById("player1");
+    // function playAud() {
+    // aud.play();
+	// }
+
+	
+  // });
+  
+  // $("#stp1").click(function(){
+	// let aud = document.getElementById("player1");
+    // function pauseAud() {
+		// aud.pause();
+	// }
+
+	
+  // });
+// });
+
+// let vid = document.getElementById("player1"); 
+
+// function playVid() { 
+  // vid.play(); 
+// } 
+
+// function pauseVid() { 
+  // vid.pause(); 
+// } 
+
+
+
